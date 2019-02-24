9554329d5d30a2db04e889c458636ff327493058
likendero <likendero11@gmail.com>
2019-02-24 22:29:20 +0100

    cambio final

--- productos/productos/models/models.py
+++ productos/productos/models/models.py
@@ -3,4 +3,14 @@
 from odoo import models, fields, api
 
 
+class productos(models.Model):
+    _name = 'productos.productos'
 
+    nombre = fields.Char()
+    stock = fields.Integer()
+    precio = fields.Float(compute="_value_pc", store=True)
+    descripcion = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
